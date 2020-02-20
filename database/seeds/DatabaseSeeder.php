0da2fd2b4120954e66abbef5425669d1a08e951a
Iva Boljevic <ivab991@gmail.com>
2020-02-20 14:14:06 +0100

    Add third homework

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(HotelsTableSeeder::class);
+        $this->call(CountriesTableSeeder::class);
     }
 }
