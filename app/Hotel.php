7d2db90913708695d9e385ef608e18f6241727d8
Iva Boljevic <ivab991@gmail.com>
2020-02-20 13:28:28 +0100

    Add second homework

---
+++ app/Hotel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    protected $table = 'hotels';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'star',
+        'street_address',
+        'city',
+        'country',
+        'created_at',
+        'updated_at',
+    ];
+    
+}
